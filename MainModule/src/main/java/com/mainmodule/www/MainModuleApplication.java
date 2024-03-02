ed03cbacbe6fc79750803ad49185434b0a2763c5
Arcerr <taurus..ali47>
2024-03-02 19:14:07 +0200

    Initial commit

---
+++ MainModule/src/main/java/com/mainmodule/www/MainModuleApplication.java
@@ -0,0 +1,13 @@
+package com.mainmodule.www;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainModuleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MainModuleApplication.class, args);
+    }
+
+}
---
+++ MainModule/src/test/java/com/mainmodule/www/MainModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mainmodule.www;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainModuleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
