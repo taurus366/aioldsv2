8be624bb483a6356c66c5e8b8351113938567582
Arcerr <taurus..ali47>
2024-04-01 00:12:38 +0300

    .

--- AioldsWeb/src/main/java/com/aioldsweb/www/web/IndexController.java
+++ AioldsWeb/src/main/java/com/aioldsweb/www/web/IndexController.java
@@ -1,16 +1,30 @@
 package com.aioldsweb.www.web;
 
+import com.aioldsweb.www.model.enums.SubjectEnum;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/")
 public class IndexController {
 
     public IndexController() {
     }
 
-    @RequestMapping("/home")
-    public String showIndexPage() {
+    @GetMapping("home")
+    public String showIndexPage(Model model) {
+
+        model
+                .addAttribute("subjects", SubjectEnum.values());
+
         return "home";
+    }
+
+    @PostMapping("/contact")
+    public void contactForm() {
+
     }
 }
